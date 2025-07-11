50ca2117468c788dadd1421b2fabb8b7890545fe
Margarita Savonevskaya <go.to.mychannel745@gmail.com>
2025-07-12 01:01:35 +0300

    fix: make pre-commit executable

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -19,6 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <h1>Vite + React</h1>
       <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
       <div className="card">
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
