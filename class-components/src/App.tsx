9e9dd31d0e46c53edb71b7b90533b113a1dc32a5
Margarita Savonevskaya <go.to.mychannel745@gmail.com>
2025-07-12 00:58:26 +0300

    feat: add husky

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -1,5 +1,6 @@
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <h1>Vite + React</h1>
       <h1>Vite + React</h1>
       <h1>Vite + React</h1>
       <h1>Vite + React</h1>
