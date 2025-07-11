6268b481f725436bbfd80bd1966958f5fd4c356c
Margarita Savonevskaya <go.to.mychannel745@gmail.com>
2025-07-11 13:51:42 +0300

    test: husky check

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -15,6 +15,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
       <div className="card">
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
