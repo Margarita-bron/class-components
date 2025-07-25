90adf68b0e999aa796933058dfaa2297e666f025
Margarita Savonevskaya <go.to.mychannel745@gmail.com>
2025-07-25 17:00:43 +0300

    feat: rewrite error button to functional component

--- class-components/src/components/errorButton/ErrorButton.tsx
+++ class-components/src/components/errorButton/ErrorButton.tsx
@@ -1,23 +1,21 @@
-import { Component, type ReactNode } from 'react';
-import type { ErrorButtonState } from '../../types/error-button-types';
+import React, { useState } from 'react';
 import './error-button.css';
 
-export class ErrorButton extends Component<object, ErrorButtonState> {
-  state = { throwError: false };
+const ErrorButton: React.FC = () => {
+  const [throwError, setThrowError] = useState(false);
 
-  handleClick = (): void => {
-    this.setState({ throwError: true });
+  if (throwError) {
+    throw new Error('Test error');
+  }
+
+  const handleClick = (): void => {
+    setThrowError(true);
   };
 
-  render(): ReactNode {
-    if (this.state.throwError) {
-      throw new Error('Test error');
-    }
-
-    return (
-      <button className="error-button" onClick={this.handleClick}>
-        CALL ERROR
-      </button>
-    );
-  }
-}
+  return (
+    <button className="error-button" onClick={handleClick}>
+      CALL ERROR
+    </button>
+  );
+};
+export default ErrorButton;
