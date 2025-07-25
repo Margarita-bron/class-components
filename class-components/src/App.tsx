6f08ff510bd6a4de183eb000e61aa1ee2a6b4fe9
Margarita Savonevskaya <go.to.mychannel745@gmail.com>
2025-07-25 19:28:03 +0300

    feat: add user hook for localstorage

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -1,48 +1,53 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import SearchBar from './components/searchBar/SearchBar';
 import { fetchBooks } from './service/books-api';
 import type { Book } from './types/books-app-types';
 import Catalog from './components/catalog/Catalog';
 import ErrorButton from './components/errorButton/ErrorButton';
+import { useLocalStorage } from './__hooks__/useLocalStorage';
 
 const App: React.FC = () => {
-  const [currentQuery, setCurrentQuery] = useState<string>('');
+  const [currentQuery, setCurrentQuery] = useLocalStorage<string>(
+    'searchQuery',
+    'book'
+  );
   const [resultData, setResultData] = useState<Book[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    setCurrentQuery(localStorage.getItem('searchQuery') || '');
     if (currentQuery) {
       handleFetchBooks(currentQuery);
     }
   }, []);
 
   const handleChangeSearchQuery = async (query: string): Promise<void> => {
-    localStorage.setItem('searchQuery', query);
     setCurrentQuery(query);
-    handleFetchBooks(query);
+    await handleFetchBooks(query);
   };
 
-  const handleFetchBooks = async (query?: string): Promise<void> => {
-    setLoading(true);
-    setError(null);
-    try {
-      const response = await fetchBooks(query ?? '');
-      setResultData(response.resultData);
-      setLoading(false);
+  const handleFetchBooks = useCallback(
+    async (query?: string): Promise<void> => {
+      setLoading(true);
       setError(null);
-    } catch (error) {
-      if (error instanceof Error) {
+      try {
+        const response = await fetchBooks(query ?? '');
+        setResultData(response.resultData);
+        setError(null);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
+        } else {
+          setError(String(error));
+        }
+      } finally {
         setLoading(false);
-        setError(error.message);
-      } else {
-        setLoading(false);
-        setError(String(error));
       }
-    }
-  };
+    },
+    []
+  );
+
   return (
     <div className="app-wrapper">
       <SearchBar
