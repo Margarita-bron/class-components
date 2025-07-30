85f1e87498153177f42056e9927bb76d91707e19
Margarita Savonevskaya <go.to.mychannel745@gmail.com>
2025-07-30 22:50:20 +0300

    feat: change header realization

--- class-components/src/components/header/Header.tsx
+++ class-components/src/components/header/Header.tsx
@@ -1,9 +1,4 @@
 import React from 'react';
-import {
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-} from '@headlessui/react';
 import { Link } from 'react-router-dom';
 import research from '../../assets/research.ico';
 
@@ -18,15 +13,10 @@ function classNames(...classes: string[]): string {
 
 const Header: React.FC = () => {
   return (
-    <Disclosure as="nav" className="header bg-gray-700">
+    <div className="header bg-gray-700">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
-              <span className="absolute -inset-0.5" />
-              <span className="sr-only">Open main menu</span>
-            </DisclosureButton>
-          </div>
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden"></div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex shrink-0 items-center">
               <img alt="Open Library" src={research} className="h-8 w-auto" />
@@ -53,28 +43,7 @@ const Header: React.FC = () => {
           </div>
         </div>
       </div>
-
-      <DisclosurePanel className="sm:hidden">
-        <div className="space-y-1 px-2 pt-2 pb-3">
-          {navigation.map((item) => (
-            <DisclosureButton
-              key={item.name}
-              as="a"
-              href={item.href}
-              aria-current={item.current ? 'page' : undefined}
-              className={classNames(
-                item.current
-                  ? 'bg-gray-900 text-white'
-                  : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                'block rounded-md px-3 py-2 text-base font-medium'
-              )}
-            >
-              {item.name}
-            </DisclosureButton>
-          ))}
-        </div>
-      </DisclosurePanel>
-    </Disclosure>
+    </div>
   );
 };
 export default Header;
